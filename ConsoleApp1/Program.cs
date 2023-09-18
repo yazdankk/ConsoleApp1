31af9bb300613c866f987fedd13890b11c40d231
AQ232466 <AQ232466@F31_05>
2023-09-18 11:01:12 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.WriteLine("What is your name?");
+                string firstName = Console.ReadLine();
+            Console.WriteLine("Hi " + firstName);
+            Random random = new Random();
+            int RNG = random.Next(1, 11);
+            Console.WriteLine("Enter a number between 1 and 10, if you guess the correct number, you win!");
+            int userGuess = Convert.ToInt32(Console.ReadLine());
+
+            while (true)
+            {
+                if (userGuess == RNG)
+                {
+                    Console.WriteLine("You guessed correct!");
+                    break;
+                }
+                else if (userGuess < RNG)
+                {
+                    Console.WriteLine("You guessed too low");
+                    userGuess = Convert.ToInt32(Console.ReadLine());
+                }
+                else
+                {
+                    Console.WriteLine("You guessed too high");
+                    userGuess = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
